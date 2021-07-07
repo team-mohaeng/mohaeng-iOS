6194162e0ed4beee8a0c7de0271cf8fccf89e6d7
chansenbbai <chanchu@hansung.ac.kr>
2021-07-07 16:55:19 +0900

    [#53] 로그인뷰 짜기
    ### Description
    - UIimage 생성
    - 이메일 / 비밀번호 텍스트필드 생성
    - 로그인 버튼 생성
    - 비밀번호 찾기 회원가입 생성
    
    Resolve #53

--- Journey/Journey/Sources/ViewControllers/Auth/LoginViewController.swift
+++ Journey/Journey/Sources/ViewControllers/Auth/LoginViewController.swift
@@ -8,23 +8,69 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet Properties
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loginImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.passwordTextField.becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        makeButtonRound()
     }
-
+    
+    // MARK: - @IBAction Functions
+    
     @IBAction func touchGoGoButton(_ sender: Any) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            let tabbarStoryboard = UIStoryboard(name: Const.Storyboard.Name.tabbar, bundle: nil)
+            guard let tabbarViewController = tabbarStoryboard.instantiateViewController(withIdentifier: Const.ViewController.Identifier.tabbar) as? TabbarViewController else {
+                return
+            }
+            tabbarViewController.modalPresentationStyle = .fullScreen
+            tabbarViewController.modalTransitionStyle = .crossDissolve
+            self.present(tabbarViewController, animated: true, completion: nil)
+        }
+    }
+    @IBAction func findPasswordButton(_ sender: Any) {
         
-        let tabbarStoryboard = UIStoryboard(name: Const.Storyboard.Name.tabbar, bundle: nil)
-        guard let tabbarViewController = tabbarStoryboard.instantiateViewController(withIdentifier: Const.ViewController.Identifier.tabbar) as? TabbarViewController else {
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        let signupFirstStoryboard = UIStoryboard(name: Const.Storyboard.Name.signUpFirst, bundle: nil)
+        
+        guard let signUpFirstViewController  = signupFirstStoryboard.instantiateViewController(withIdentifier: Const.ViewController.Identifier.signUpFirst) as? SignUpFirstViewController else {
             return
         }
-        
-        tabbarViewController.modalPresentationStyle = .fullScreen
-        tabbarViewController.modalTransitionStyle = .crossDissolve
-        self.present(tabbarViewController, animated: true, completion: nil)
-        
+        self.navigationController?.pushViewController(signUpFirstViewController, animated: true)
+    }
+    
+    // MARK: - Functions
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.passwordTextField.resignFirstResponder()
+    }
+    
+    private func makeButtonRound() {
+        loginButton.makeRounded(radius: 20)
+    }
+    
+}
+
+// MARK: - UITextFieldDelegate
+
+extension LoginViewController: UITextFieldDelegate {
+    func passwordTextFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.passwordTextField.resignFirstResponder()
+        self.dismiss(animated: true, completion: nil)
+        return true
     }
 }
