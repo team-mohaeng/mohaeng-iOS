283b878b1d1ff45d9ea15557532e6c932577f0c9
iamcho2 <thatgrass@naver.com>
2021-07-10 04:06:29 +0900

    [#41] URL Constant 정리하기
    ### Description
    - Moya 사용을 고려한 URL Constant 추가
    - 아직 안 나온 api (PW찾기, 소확행 삭제, 푸시알림)은 추가하지 않음
    
    ### Related Issue
    Resolve #41

--- Journey/Journey/Resources/Constants/URL.swift
+++ Journey/Journey/Resources/Constants/URL.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+extension Const {
+    struct URL {
+        
+        // base url
+        static let baseURL = "https://3.36.55.247:5000/api"
+        
+        // MARK: - Auth
+        
+        // 회원가입 (POST)
+        static let signUpURL = "/signup"
+        
+        // 로그인 (POST)
+        static let signInURL = "/signin"
+        
+        // 비밀번호 찾기 (POST)
+        
+        // MARK: - HOME
+        
+        // 홈 (GET)
+        static let homeURL = "/home"
+        
+        // MARK: - Challenge
+        
+        // 코스 전체 조회 (GET)
+        static let coursesURL = "/courses"
+        
+        // 코스 진행하기 (PUT) : "/courses/:id"
+        
+        // 전체 챌린지 지도 조회 (GET)
+        static let challengesURL = "/challenges"
+        
+        // 오늘의 챌린지 조회 (GET), 챌린지 인증 (PUT) :
+        // "/challenges/:courseId/:challengeId
+        
+        // 완료한 코스 메달 (GET)
+        static let medalURL = "/course/complete"
+        
+        // MARK: - 소확행
+        
+        // 소확행 입력(POST), 커뮤니티(GET)
+        static let happyURL = "/smallSatisfaction"
+        
+        // 내 서랍장(GET)
+        static let myDrawerURL = "/myDrawer" // "/myDrawer/:month"
+        
+        // 상세보기(GET), 좋아요(PUT), 좋아요 취소(PUT) : "/smallSatisfaction/:postId"
+    }
+}
