015503bf700656c53e0cf98fa39f429f0b718ef1
yejiyun <20172062@sungshin.ac.kr>
2021-07-11 02:43:51 +0900

    [#82] 해시태그 로직 추가
    ### Description
    - 해시태그 최대 개수 제한 -> 팝업
    - 해시태그 X 버튼 클릭 시 삭제
    
    ### Related Issue
    Resolve #82

--- Journey/Journey/Sources/Cells/FeedView/HashTagCollectionViewCell.swift
+++ Journey/Journey/Sources/Cells/FeedView/HashTagCollectionViewCell.swift
@@ -9,16 +9,38 @@ import UIKit
 
 class HashTagCollectionViewCell: UICollectionViewCell {
 
-    // MARK: - IBOutlet Properties
+    // MARK: - @IBOutlet Properties
+    
     @IBOutlet weak var keywordLabel: UILabel!
+    
+    
+    // MARK: - View Life Cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
     
+    // MARK: - fuction
+    
     func setData(keyword: String?) {
         keywordLabel.text = keyword
     }
     
+    func getIndexPath() -> Int {
+        var indexPath = 0
+        
+        guard let superView = self.superview as? UICollectionView else {
+            return -1
+        }
+        indexPath = superView.indexPath(for: self)!.row
+        
+        return indexPath
+    }
+    
+    // MARK: - @IBAction Properties
+    
+    @IBAction func touchRemoveButton(_ sender: Any) {
+        NotificationCenter.default.post(name: NSNotification.Name("hashTagRemoveButtonClicked"), object: getIndexPath())
+    }
 }
