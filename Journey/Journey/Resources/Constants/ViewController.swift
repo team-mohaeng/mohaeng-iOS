6948fa44014ba93ba8fabf31d4b00071061c9119
iamcho2 <thatgrass@naver.com>
2021-07-11 16:48:00 +0900

    [#98] 인증코드 뷰 짜기
    ### Description
    - 인증코드 뷰
    
    ### Related Issue
    Resolve #98

--- Journey/Journey/Resources/Constants/ViewController.swift
+++ Journey/Journey/Resources/Constants/ViewController.swift
@@ -17,6 +17,7 @@ extension Const {
             // Auth
             static let login = "LoginViewController"
             static let signUpFirst = "SignUpFirstViewController"
+            static let code = "CodeViewController"
             // 홈 탭
             static let home = "HomeViewController"
             static let medal = "MedalViewController"
