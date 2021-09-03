b0ad8be69d2c6d1d1052c723a37bb08fd1839cfd
yejiyun <20172062@sungshin.ac.kr>
2021-09-03 14:20:17 +0900

    [#292] mask 크기 조정
    - shadow radius가 커지면 잘리는 거 방지하기 위해서 범위 조정

--- Journey/Journey/Sources/Extensions/UIView+Extension.swift
+++ Journey/Journey/Sources/Extensions/UIView+Extension.swift
@@ -47,19 +47,20 @@ extension UIView {
     
     func dropShadow(rounded: CGFloat) {
         let margin = 24
+        let maximumRadiusValue: CGFloat = 50
         
         /// layer 생성 후 크기 설정
         let maskLayer = CAShapeLayer()
         var bounds = self.bounds
         if self.frame.width > self.frame.height {
-            /// corse info view에서 width bounds가 제대로 인식이 안되는 이슈로 인해 양옆 margin 값 직접 넣어줌
+            /// course info view에서 width bounds가 제대로 인식이 안되는 이슈로 인해 양옆 margin 값 직접 넣어줌
             bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - CGFloat(margin * 2), height: self.bounds.height)
         }
         maskLayer.frame = bounds
         maskLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: self.layer.cornerRadius).cgPath
         
         /// mask 영역을 뷰보다 크게 만들어서 mask에 의해 shadow가 잘리지 않도록 함
-        let shadowBorder: CGFloat = self.layer.shadowRadius * 2 + 20
+        let shadowBorder: CGFloat = self.layer.shadowRadius * 2 + maximumRadiusValue
         maskLayer.frame = maskLayer.frame.insetBy(dx: -shadowBorder, dy: -shadowBorder)
         maskLayer.frame = maskLayer.frame.offsetBy(dx: shadowBorder / 2, dy: shadowBorder / 2)
         
@@ -73,7 +74,7 @@ extension UIView {
 
         self.layer.mask = maskLayer
         self.layer.shadowColor = UIColor.darkGray.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowOffset = .zero
         self.layer.shadowRadius = rounded
         self.layer.shadowOpacity = 0.1
         self.layer.masksToBounds = false
